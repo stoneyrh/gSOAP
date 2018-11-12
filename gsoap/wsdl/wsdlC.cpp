3ea7cbd5a6cff8cf2417807b97e691876b761d44
engelen <engelen@06ac4634-b970-40ce-84dc-3611c6d995e3>
2018-11-12 21:49:52 +0000

    gSOAP 2.8.71 stable
    git-svn-id: https://svn.code.sf.net/p/gsoap2/code@148 06ac4634-b970-40ce-84dc-3611c6d995e3

--- gsoap/wsdl/wsdlC.cpp
+++ gsoap/wsdl/wsdlC.cpp
@@ -18,7 +18,7 @@ A commercial use license is available from Genivia Inc., contact@genivia.com
 
 #include "wsdlH.h"
 
-SOAP_SOURCE_STAMP("@(#) wsdlC.cpp ver 2.8.71 2018-11-12 21:20:27 GMT")
+SOAP_SOURCE_STAMP("@(#) wsdlC.cpp ver 2.8.71 2018-11-12 21:47:09 GMT")
 
 
 #ifndef WITH_NOGLOBAL
